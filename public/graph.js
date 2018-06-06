32309d0ce46a562298dcfea52dde682249200a57
Robert Skarżycki <3417357+robert-skarzycki@users.noreply.github.com>
2018-06-06 07:09:04 +0200

    In the middle of switching to react

--- public/graph.js
+++
@@ -1,7 +0,0 @@
-var graph = Viva.Graph.graph();
-graph.addLink(1, 2);
-
-var renderer = Viva.Graph.View.renderer(graph, {
-    container: document.getElementsByClassName('graph')
-});
-renderer.run();
